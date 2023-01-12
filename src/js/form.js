99c6516ae28ec94e2f7156914e3f72f660c1311f
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-01-12 03:16:45 +0200

    add homework-11

---
+++ src/js/form.js
@@ -0,0 +1,133 @@
+import Notiflix from 'notiflix';
+import cardTpl from '../templates/cards.hbs';
+import GalleryServis from './gallery_servis';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryServis = new GalleryServis();
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('[name="searchQuery"]'),
+  submitBtn: document.querySelector('[type="submit"]'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  sentinel: document.querySelector('#sentinel'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
+
+async function onFormSubmit(e) {
+  e.preventDefault();
+
+  let inputValue = e.currentTarget.elements.searchQuery.value;
+
+  refs.loadMoreBtn.style.display = 'none';
+
+  if (!inputValue) {
+    renderClear(refs.gallery);
+    return Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  galleryServis.query = inputValue;
+  galleryServis.resetPage();
+
+  const { photoArray, totalPhotos } = await galleryServis.getPhotos();
+
+  if (photoArray.length === 0) {
+    renderClear(refs.gallery);
+    return Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  renderClear(refs.gallery);
+  cardRenderMurkup(photoArray);
+
+  refs.loadMoreBtn.style.display = 'block';
+
+  addSimpleLightbox();
+}
+
+async function onClickLoadMoreBtn() {
+  const { photoArray, totalPhotos } = await galleryServis.getPhotos();
+
+  if (refs.gallery.children.length === totalPhotos) {
+    refs.loadMoreBtn.style.display = 'none';
+    return Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  cardRenderMurkup(photoArray);
+
+  addSimpleLightbox();
+
+  scroll();
+}
+
+function cardRenderMurkup(array) {
+  const markup = array.map(card => cardTpl(card)).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderClear(element) {
+  const markup = ``;
+  element.innerHTML = markup;
+}
+
+function addPreventDefaultLink(link) {
+  return link.addEventListener('click', e => {
+    e.preventDefault();
+  });
+}
+
+function addSimpleLightbox() {
+  const linksEl = document.querySelectorAll('.photo');
+  linksEl.forEach(link => {
+    addPreventDefaultLink(link);
+  });
+  let lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+}
+
+function scroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight,
+    behavior: 'smooth',
+  });
+}
+
+function onEntry(entryes) {
+  entryes.forEach(async entry => {
+    if (entry.isIntersecting && galleryServis.query !== '') {
+      const { photoArray, totalPhotos } = await galleryServis.getPhotos();
+
+      cardRenderMurkup(photoArray);
+
+      addSimpleLightbox();
+
+      if (refs.gallery.children.length === totalPhotos) {
+        refs.loadMoreBtn.style.display = 'none';
+        return Notiflix.Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+    }
+  });
+}
+
+const options = {
+  rootMargin: '150px',
+};
+
+const observer = new IntersectionObserver(onEntry, options);
+observer.observe(refs.sentinel);
---
+++ src/js/gallery_servis.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const PIXABAY_URL = 'https://pixabay.com/api/';
+const options = {
+  key: '32614456-fa2d97d1002b2d5ec83882b58',
+};
+
+export default class GalleryServis {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.showNumberOfPhotos = 40;
+  }
+
+  async getPhotos() {
+    const url = `${PIXABAY_URL}?key=${options.key}&q=${this.searchQuery}&page=${this.page}&per_page=${this.showNumberOfPhotos}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    try {
+      const response = await axios.get(url);
+      const photoArray = response.data.hits;
+      const totalPhotos = response.data.totalHits;
+
+      this.page += 1;
+
+      return { photoArray, totalPhotos };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
+}
